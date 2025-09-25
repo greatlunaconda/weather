1d6733c4e5c39e54158cd3a2f72e89f56fcca8ad
kinugawa <lunaconda915@gmail.com>
2025-09-25 23:36:17 +0900

    after Menu component Amazon Q

--- app/ui/menu.tsx
+++ app/ui/menu.tsx
@@ -1,15 +1,54 @@
 'use client';
 
+import { Linden_Hill } from "next/font/google";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Menu() {
-  const [showMenu, setShowMenu] = useState('hidden');
+  const [activeMenu, setActiveMenu] = useState('');
 
-  const handleClick  (e) =>  {
-    setShowMenu('show');
-  }
+  const handleMenuClick = (menuName: string) => {
+    setActiveMenu(activeMenu === menuName ? '' : menuName);
+  };
+
   return (
-    
-  )
-  
+    <div className="menu-container">
+      <div className="menu-items">
+        <button 
+          onClick={() => handleMenuClick('selectPlace')}
+          className="menu-button"
+        >
+          Select pl
+        </button>
+        {activeMenu === 'selectPlace' && (
+          <div className="menu-content">
+            <Link href="/leaflet-place-selector">Select Place</Link>
+          </div>
+        )}
+         <button>
+          onClick={() => handleMenuClick('language')}
+          className="menu-button"
+        >
+          Language
+        </button>
+        {activeMenu === 'language' && (
+          <div className="menu-content">
+            <p>Language content</p>
+          </div>
+        )}
+        
+        <button 
+          onClick={() => handleMenuClick('aboutUs')}
+          className="menu-button"
+        >
+          About us
+        </button>
+        {activeMenu === 'aboutUs' && (
+          <div className="menu-content">
+            <p>About us content</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
